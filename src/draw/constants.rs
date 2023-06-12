7dcc65674e7d83bd606db0732a67aab4aa8d7db7
Karl Lukan <klukan20@gmail.com>
2023-06-11 23:05:35 -0500

    added discriminator and player puck

---
+++ src/draw/constants.rs
@@ -0,0 +1,7 @@
+pub const TITLE: &str = "PRAWNG";
+
+pub const WINDOW_HEIGHT: u32 = 600;
+pub const WINDOW_WIDTH: u32 = 800;
+
+pub const PUCK_WIDTH: u32 = 16;
+pub const PUCK_HEIGHT: u32 = 82;
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use sdl2::{pixels::Color, event::Event, keyboard::Keycode};
+use draw::{constants::{WINDOW_WIDTH, WINDOW_HEIGHT, TITLE}, game_objects::{draw_rectangle, draw_discriminator}};
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Point};
 
-const TITLE: &str = "PRAWNG";
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+mod draw;
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window(TITLE, WIDTH, HEIGHT)
+        .window(TITLE, WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
@@ -21,22 +20,33 @@ fn main() -> Result<(), String> {
         .build()
         .map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.set_draw_color(Color::WHITE);
 
-    canvas.clear();
-    canvas.present();
-
+    let mut player_position = WINDOW_HEIGHT / 2;
+    
     'running: loop {
         for event in sdl_context.event_pump()?.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
+                Event::Quit { .. } | Event::KeyDown {
                     keycode: Option::Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                
+                Event::KeyDown {
+                    keycode: Option::Some(Keycode::Down) | Option::Some(Keycode::S),
+                    ..
+                } => { 
+                    player_position += 8;
+                }
+                
                 _ => {}
             }
         }
+        
+        draw_discriminator(&mut canvas)?;
+        draw_rectangle(&mut canvas, Point::new(WINDOW_WIDTH as i32 - 32, player_position as i32))?;
+        
+        canvas.present();
     }
 
     Ok(())
