968e00cad3ceb6c01a8efa780375b5f5720050df
Karl Lukan <klukan20@gmail.com>
2023-06-22 00:00:56 -0500

    added tests for Paddle.move_position

--- src/game/paddle.rs
+++ src/game/paddle.rs
@@ -26,3 +26,51 @@ impl Paddle {
         self.position = position;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use sdl2::rect::Point;
+    use crate::game::paddle::Paddle;
+
+    #[test]
+    fn cannot_move_above() {
+        let original_pos = Point::new(400, 300);
+        let new_pos = Point::new(400, 600);
+        let mut paddle = Paddle::new(original_pos);
+
+        paddle.move_position(new_pos);
+
+        assert_eq!(
+            original_pos,
+            paddle.get_position()
+        );
+    }
+
+    #[test]
+    fn can_move_within_bounds() {
+        let original_pos = Point::new(400, 300);
+        let new_pos = Point::new(400, 308);
+        let mut paddle = Paddle::new(original_pos);
+
+        paddle.move_position(new_pos);
+
+        assert_ne!(
+            original_pos,
+            paddle.get_position()
+        );
+    }
+
+    #[test]
+    fn cannot_move_below() {
+        let original_pos = Point::new(400, 300);
+        let new_pos = Point::new(400, 0);
+        let mut paddle = Paddle::new(original_pos);
+
+        paddle.move_position(new_pos);
+
+        assert_eq!(
+            original_pos,
+            paddle.get_position()
+        );
+    }
+}
